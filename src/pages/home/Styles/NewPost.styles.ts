16c1f2d15e733561fe43833431fd6543c8b92a13
OhSSangHoon <gnsdkaos99@naver.com>
2024-11-12 04:57:10 +0900

    new post realize

--- src/pages/home/Styles/NewPost.styles.ts
+++ src/pages/home/Styles/NewPost.styles.ts
@@ -1,43 +1,104 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  height: auto;
+  width: 100%;
   min-height: 100vh;
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
-  color: white;
+  color: #fff;
   position: relative;
 `;
 
 export const Title = styled.h1`
-  font-size: 2.5rem;
-  margin-bottom: 50px;
+  margin-top: 100px;
+  font-size: 3em;
+  font-weight: 300;
+  text-align: center;
+  color: #fff;
 `;
 
-export const PostList = styled.ul`
-  list-style: none;
-  padding: 0;
-  margin: 0;
+export const HashInt = styled.div`
+    width: 100%;
+    color: #fff;
+    margin: 0 auto;
+    margin-top: 5px;
+    margin-bottom: 100px;
+    text-align: center;
+    font-size: 1.2em;
+    font-weight: 100;
+`
+
+export const PostList = styled.div`
   width: 80%;
-  max-width: 800px;
+  margin: 0 auto;
+  display: flex;
+  will-change: transform;
+  overflow: visible;
 `;
 
-export const PostItem = styled.li`
-  display: flex;
-  justify-content: space-between;
-  border-bottom: 1px solid #444;
-  padding: 15px 0;
+export const PostItem = styled.div`
+  flex: 0 0 calc(100% / 5 - 16px);
+  max-width: calc(100% / 5 - 16px);
+  height: 400px;
+  background-color: #1a1a1b;
+  border-radius: 8px;
+  margin: 0 8px;
+  padding: 20px;
   color: #bbb;
+  text-align: center;
+  box-sizing: border-box;
+  cursor: pointer;
+  transition: transform 0.3s ease, width 0.3s ease, box-shadow 0.3s ease;
 
   & > span {
     font-size: 0.875rem;
   }
+
+  &:hover {
+    transform: translateY(-10px) scale(1.05);
+    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+  }
 `;
 
-export const DotNavWrapper = styled.div`
-  position: absolute;
-  right: 80px;
-  bottom: 100px;
+
+export const PostTitle = styled.p`
+  text-align: left;
+  margin-top: 10px;
+  color: #fff;
+  font-size: 1.2em;
+`;
+
+export const thumbnail = styled.img`
+  width: 100%;
+  height: 150px;
+  object-fit: cover;
+  border-radius: 8px;
+`;
+
+export const PostContents = styled.p`
+  margin: 10px 0;
+  text-align: left;
+`
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 10%;
+  margin-top: 20px;
+`;
+
+export const LoadMoreButton = styled.button`
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+
+  &:disabled {
+    background-color: #ddd;
+    cursor: not-allowed;
+  }
 `;
